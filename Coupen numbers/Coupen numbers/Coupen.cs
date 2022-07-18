8d3faf99f7cb4ef5ab8c0950d635eaeb6c9957a0
srisri96 <srikanthsitharth80@gmil.com>
2022-07-18 18:22:07 +0530

    coupen numbers

---
+++ Coupen numbers/Coupen numbers/Coupen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coupen_numbers
+{
+    internal class Coupen
+    {
+
+        public static int random()
+        {
+            Random random = new Random();
+            int r = random.Next(1, 10);
+            return r;
+        }
+
+        public static void coup5(int n)
+        {
+            int[] ar = new int[n];
+            for (int i = 0; i < n; i++)             
+            {
+                ar[i] = random();
+                for (int j = 0; j < i; j++)        
+                {
+                    if (ar[i] == ar[j])            
+                    {
+                        ar[i] = random();              
+                        i--;              
+                        break;           
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(ar[i] + "  ");
+            }
+        }
+    }
+}
---
+++ Coupen numbers/Coupen numbers/Program.cs
@@ -0,0 +1,5 @@
+﻿using Coupen_numbers;
+Console.WriteLine("Prints distict coupon numbers from 1 to 20");
+Console.WriteLine("Enter a n for number of distinct numbers");
+int num = Convert.ToInt32(Console.ReadLine());
+Coupen.coup5(num);
